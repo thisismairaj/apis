ca4cde491b4d121d0230987e6288d3e1f27d73da
Mairaj <thisismairaj@gmail.com>
2017-09-08 20:24:25 +0500

    Adds basic structure, bootstrap 3

--- cdnjs/index.php
+++ cdnjs/index.php
@@ -3,13 +3,47 @@
 <head>
 	<meta charset="UTF-8">
 	<title>CDNJS API</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<style>
+	#header{
+		padding: 30px;
+	}
+	.logo a {
+		color:white;
+	}
+	</style>
 </head>
 <body>
 
-<form action="index.php" method="post">
-	<input type="text" name="library">
-	<input type="submit" name="submit">
-</form>
+<div class="container">
+	<div class="bg-primary">
+		<header id="header">
+			<h1 class="logo"><a href="#">CDNJS API</a></h1>
+		</header>
+	</div>
+
+<div class="jumbotron">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<h1>Welcome to the site!</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem necessitatibus vero nisi id nulla aliquid tempora, quam ipsam excepturi, facilis obcaecati, sunt inventore numquam praesentium saepe itaque ducimus voluptas dignissimos!</p>
+				<br>
+			</div>
+			<div class="col-md-4">
+				<form action="index.php" method="post">
+					<div class="form-group">
+					<label>Library Name: </label>
+						<input class="form-control" type="text" name="library">
+					</div>
+					<div class="form-group">
+						<input class="form-control btn btn-primary" type="submit" name="submit">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php
 
@@ -22,6 +56,6 @@ if(isset($_POST['submit'])) {
 	}
 }
 ?>
-	
+</div>
 </body>
 </html>
